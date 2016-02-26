9e4a41b37113291d2a6d8af2a1e631b0be1f5953
andyrutkowskiucla <arutkowski@library.ucla.edu>
2016-02-25 23:58:02 -0800

    Update base-map.js

--- js/base-map.js
+++ js/base-map.js
@@ -1,8 +1,8 @@
 // Set view of Leaflet map based on screen size
 var layer = new L.StamenTileLayer('toner-background');
 if ($(window).width() < 626) {
-	var map = new L.Map('map').setView([33,-118],6);
+	var map = new L.Map('map').setView([42,-93],6);
 } else {
-	var map = new L.Map('map').setView([33,-118],7);
+	var map = new L.Map('map').setView([42,-93],7);
 }
 map.addLayer(layer);
