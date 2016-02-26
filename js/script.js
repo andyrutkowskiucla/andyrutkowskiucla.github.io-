2798ea70622ee0a8536ca4dd75c7d802beba7e7b
andyrutkowskiucla <arutkowski@library.ucla.edu>
2016-02-25 23:26:48 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,8 +38,8 @@ function generatePopup(content){
 	popup_content += '<td>' + content['Date'] + '</td>';
 	popup_content += '<tr><td><strong>About URL</strong></td>';
 	popup_content += '<td>' + content['About URL'] + '</td>';
-	popup_content += '<tr><td><strong>Address</strong></td>';
-	popup_content += '<td>' + content['Address'] + '</td>';
+	popup_content += '<tr><td><strong>Caption</strong></td>';
+	popup_content += '<td>' + content['Caption'] + '</td>';
 		popup_content += '<tr><td colspan="2"><strong><a href="http://' + content['website'] + '" target="_blank">Learn more</a></strong></td>';
 	popup_content += '</tr></table>'
 
